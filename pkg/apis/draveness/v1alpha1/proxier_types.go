6b469703c0c2534ffe20021a159d2b095158ca1b
draveness <i@draveness.me>
2019-03-14 17:23:45 +0800

    feat: remove comments in proxier types

--- pkg/apis/draveness/v1alpha1/proxier_types.go
+++ pkg/apis/draveness/v1alpha1/proxier_types.go
@@ -4,9 +4,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // ProxierSpec defines the desired state of Proxier
 // +k8s:openapi-gen=true
 type ProxierSpec struct {
