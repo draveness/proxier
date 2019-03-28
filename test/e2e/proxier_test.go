e76cf8bff44cf8b3a547a519d49366d980df6120
draveness <i@draveness.me>
2019-03-28 16:45:55 +0800

    test: create backend service

--- test/e2e/proxier_test.go
+++ test/e2e/proxier_test.go
@@ -1,7 +1,12 @@
 package e2e
 
 import (
+	"errors"
 	"testing"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 func testCreateBasicProxier(t *testing.T) {
@@ -15,6 +20,25 @@ func testCreateBasicProxier(t *testing.T) {
 	exampleProxier := framework.MakeBasicProxier(ns, "test", []string{"v1", "v2"}, []int32{100, 10})
 
 	if _, err := framework.CreateProxierAndWaitUntilReady(ns, exampleProxier); err != nil {
+		t.Fatal(err)
+	}
+
+	err := wait.Poll(5*time.Second, 30*time.Second, func() (bool, error) {
+		svcList, err := framework.KubeClient.CoreV1().Services(ns).List(metav1.ListOptions{
+			LabelSelector: "maegus.com/proxier-name=" + exampleProxier.Name,
+		})
+		if err != nil {
+			return false, err
+		}
+
+		if len(svcList.Items) != 2 {
+			return false, errors.New("proxier should create backend services")
+		}
+
+		return true, nil
+	})
+
+	if err != nil {
 		t.Fatal(err)
 	}
 }
