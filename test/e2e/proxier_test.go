47751ab6e073fd07388f49a250f11fd4f2a13100
draveness <i@draveness.me>
2019-04-09 21:26:26 +0800

    test: create namespace in each tests separately

--- test/e2e/proxier_test.go
+++ test/e2e/proxier_test.go
@@ -9,35 +9,50 @@ import (
 
 type ProxierCreateSuite struct {
 	suite.Suite
-	namespace string
 }
 
-func (suite *ProxierCreateSuite) SetupTest() {
+func (suite *ProxierCreateSuite) TestProxierCreateBackends() {
 	ctx := framework.NewTestCtx(suite.T())
 	defer ctx.Cleanup(suite.T())
-	ns := ctx.CreateNamespace(suite.T(), framework.KubeClient)
-	ctx.SetupProxierRBAC(suite.T(), ns, framework.KubeClient)
-}
 
-func (suite *ProxierCreateSuite) testProxierCreateBackends() {
+	namespace := ctx.CreateNamespace(suite.T(), framework.KubeClient)
+	ctx.SetupProxierRBAC(suite.T(), namespace, framework.KubeClient)
+
 	suite.T().Parallel()
 
-	exampleProxier := framework.MakeBasicProxier(suite.namespace, "test", []string{"v1", "v2"}, []int32{100, 10})
+	exampleProxier := framework.MakeBasicProxier(namespace, "test", []string{"v1", "v2"}, []int32{100, 10})
 
-	_, err := framework.CreateProxierAndWaitUntilReady(suite.namespace, exampleProxier)
+	_, err := framework.CreateProxierAndWaitUntilReady(namespace, exampleProxier)
 
 	assert.Nil(suite.T(), err, "create proxier error")
 
-	svcList, err := framework.KubeClient.CoreV1().Services(suite.namespace).List(metav1.ListOptions{
+	svcList, err := framework.KubeClient.CoreV1().Services(namespace).List(metav1.ListOptions{
 		LabelSelector: "maegus.com/proxier-name=" + exampleProxier.Name,
 	})
 
 	assert.Nil(suite.T(), err, "list service error")
 	assert.Equal(suite.T(), 2, len(svcList.Items), "proxier should create backend services")
+}
+
+func (suite *ProxierCreateSuite) TestProxierCreateNginxDeployment() {
+	ctx := framework.NewTestCtx(suite.T())
+	defer ctx.Cleanup(suite.T())
+
+	namespace := ctx.CreateNamespace(suite.T(), framework.KubeClient)
+	ctx.SetupProxierRBAC(suite.T(), namespace, framework.KubeClient)
+
+	suite.T().Parallel()
+
+	exampleProxier := framework.MakeBasicProxier(namespace, "test", []string{"v1", "v2"}, []int32{100, 10})
+
+	_, err := framework.CreateProxierAndWaitUntilReady(namespace, exampleProxier)
+
+	assert.Nil(suite.T(), err, "create proxier error")
 
 	deploymentName := proxier.NewDeploymentName(exampleProxier)
-	deployment, err := framework.KubeClient.AppsV1().Deployments(suite.namespace).Get(deploymentName, metav1.GetOptions{})
+	deployment, err := framework.KubeClient.AppsV1().Deployments(namespace).Get(deploymentName, metav1.GetOptions{})
 
 	assert.Nil(suite.T(), err, "get deployment error")
+	assert.Equal(suite.T(), "nginx", deployment.Spec.Template.Spec.Containers[0].Name, "invalid nginx name")
 	assert.Equal(suite.T(), "nginx:1.15.9", deployment.Spec.Template.Spec.Containers[0].Image, "invalid nginx image")
 }
