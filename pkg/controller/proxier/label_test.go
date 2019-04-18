ce0780ae62217d70bbe853447f5e3e09a031a6c4
draveness <i@draveness.me>
2019-04-18 13:07:50 +0800

    test: add pod label test

--- pkg/controller/proxier/label_test.go
+++ pkg/controller/proxier/label_test.go
@@ -1,6 +1,24 @@
 package proxier
 
-import "testing"
+import (
+	"testing"
+
+	maegusv1 "github.com/draveness/proxier/pkg/apis/maegus/v1"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 func TestNewPodLabel(t *testing.T) {
+	proxier := &maegusv1.Proxier{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+	}
+
+	labels, err := newPodLabel(proxier)
+	assert.Nil(t, err)
+	assert.Equal(t, map[string]string{
+		maegusv1.ProxierKeyLabel: "default/test",
+	}, labels)
 }
