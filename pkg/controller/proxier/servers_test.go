72025ac43d58ec92d1e03ed153e8f12c7de79a63
draveness <i@draveness.me>
2019-04-19 15:09:47 +0800

    test: add unit test to verify group servers

---
+++ pkg/controller/proxier/servers_test.go
@@ -0,0 +1,83 @@
+package proxier
+
+import (
+	"testing"
+
+	maegusv1 "github.com/draveness/proxier/pkg/apis/maegus/v1"
+
+	"github.com/draveness/proxier/test/framework"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestGroupServers(t *testing.T) {
+	instance := framework.MakeBasicProxier("default", "group-server", []string{"v1", "v2"}, []int32{10, 20})
+
+	existingServices := []*corev1.Service{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "group-server-v1-backend",
+				Namespace: "default",
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "group-server-v2-backend",
+				Namespace: "default",
+			},
+		},
+	}
+
+	servicesToAdd, servicesToDelete := groupServers(instance, existingServices)
+
+	assert.Equal(t, []*corev1.Service{}, servicesToDelete)
+	assert.Equal(t, []*corev1.Service{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "group-server-v1-backend",
+				Namespace: "default",
+				Labels: map[string]string{
+					"maegus.com/proxier-name": instance.Name,
+				},
+			},
+			Spec: corev1.ServiceSpec{
+				Selector: map[string]string{
+					"version": "v1",
+					"app":     "group-server",
+				},
+				Type: corev1.ServiceTypeClusterIP,
+				Ports: []corev1.ServicePort{
+					{
+						Name:     "http",
+						Protocol: corev1.Protocol(maegusv1.ProtocolTCP),
+						Port:     80,
+					},
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "group-server-v2-backend",
+				Namespace: "default",
+				Labels: map[string]string{
+					"maegus.com/proxier-name": instance.Name,
+				},
+			},
+			Spec: corev1.ServiceSpec{
+				Selector: map[string]string{
+					"version": "v2",
+					"app":     "group-server",
+				},
+				Type: corev1.ServiceTypeClusterIP,
+				Ports: []corev1.ServicePort{
+					{
+						Name:     "http",
+						Protocol: corev1.Protocol(maegusv1.ProtocolTCP),
+						Port:     80,
+					},
+				},
+			},
+		},
+	}, servicesToAdd)
+}
