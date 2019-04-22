fa8d8e6c2e8ee8c816b805ccda14e355c5a3ecb9
draveness <i@draveness.me>
2019-04-22 20:06:23 +0800

    chore: add todo in docker image exists

--- hack/docker_image_exists.sh
+++ hack/docker_image_exists.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# TODO: use pkg md5 hash instead of git commit sha256
 OPERATOR_E2E_IMAGE_TAG=`git rev-parse --short HEAD`
 curl --silent -f -lSL https://index.docker.io/v1/repositories/draveness/proxier-e2e/tags/$OPERATOR_E2E_IMAGE_TAG > /dev/null
